704533f02ea00ecb85de31b9ef43648bd37988df
ZachBeimford <zacharybeimford@gmail.com>
2023-09-20 13:21:49 -0700

    challenge 2 push
    also fixing behaviors

--- interview/order/views.py
+++ interview/order/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from interview.order.models import Order, OrderTag
 from interview.order.serializers import OrderSerializer, OrderTagSerializer
@@ -13,3 +14,11 @@ class OrderListCreateView(generics.ListCreateAPIView):
 class OrderTagListCreateView(generics.ListCreateAPIView):
     queryset = OrderTag.objects.all()
     serializer_class = OrderTagSerializer
+
+class DeactivateOrderView(APIView):
+    def post(self, request, pk):
+        deactivated_order = Order.objects.get(pk=pk)
+        if deactivated_order.is_active:
+            deactivated_order.deactivate()
+        deactivated_order.save()
+        return Response(status=202)
