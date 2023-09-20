ce6533d99a45d8edea6a663a450b852359f11cea
ZachBeimford <zacharybeimford@gmail.com>
2023-09-20 13:42:06 -0700

    challenge 3

--- interview/order/urls.py
+++ interview/order/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-from interview.order.views import OrderListCreateView, OrderTagListCreateView
+from interview.order.views import OrderListCreateView, OrderTagListCreateView, OrdersBetweenStartDateEmbargoDateView
 
 
 urlpatterns = [
     path('tags/', OrderTagListCreateView.as_view(), name='order-detail'),
     path('', OrderListCreateView.as_view(), name='order-list'),
-
+    path('preembargo/startdate/<int:start_year>/<int:start_month>/<int:start_day>/embargodate/<int:embargo_year>/<int'
+         ':embargo_month>/<int:embargo_day>/', OrdersBetweenStartDateEmbargoDateView.as_view(), name='pre-embargo-list')
 ]
